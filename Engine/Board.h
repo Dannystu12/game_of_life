418f35bf0056a120569953b612444f76f1ec5627
Dannystu12 <mgdog1@googlemail.com>
2018-09-20 15:26:45 +0100

    Update board size

--- Engine/Board.h
+++ Engine/Board.h
@@ -11,11 +11,11 @@ public:
 	void DrawBoard();
 	static bool IsValidLocation(Location l);
 	static Location ConvertCoordToLocation(int x, int y);
-	static constexpr int width = 27;
-	static constexpr int height = 27;
+	static constexpr int width = 38;
+	static constexpr int height = 38;
 private:
 	Graphics& gfx;
-	static constexpr int dimension = 20;
+	static constexpr int dimension = 15;
 	static constexpr Color c = Colors::White;
 	static constexpr Color bc = Color(60, 60, 60);
 	static const int xPadding = Graphics::ScreenWidth / 2 - width/2 * dimension;
